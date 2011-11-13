098e605b6ed901802848ef81f27054ffacf423d2
Hong Xu <xuhdev@gmail.com>
2011-11-13 18:38:28 +0800

    Restructure the code in the "handler" function to be ready for "preprocess" feature implementation.

--- src/lib/editorconfig.c
+++ src/lib/editorconfig.c
@@ -30,36 +30,6 @@
 #include "ini.h"
 #include "ec_fnmatch.h"
 
-typedef struct
-{
-    char*       filename;
-} configuration;
-
-typedef struct
-{
-    configuration                   conf;
-
-    editorconfig_name_value*        out_values;
-    int                             current_value_count;
-    int                             max_value_count;
-} handler_first_param;
-
-/*
- * Find the editorconfig_name_value from name in a editorconfig_name_value
- * array.
- */
-static int find_name_value_from_name(const editorconfig_name_value* env,
-        int count, const char* name)
-{
-    int         i;
-
-    for (i = 0; i < count; ++i)
-        if (!strcmp(env[i].name, name)) /* found */
-            return i;
-
-    return -1;
-}
-
 /*
  * Set the name and value of a editorconfig_name_value structure
  */
@@ -77,74 +47,130 @@ static void set_name_value(editorconfig_name_value* nv, const char* name,
 }
 
 /*
+ * Find the editorconfig_name_value from name in a editorconfig_name_value
+ * array.
+ */
+static int find_name_value_from_name(const editorconfig_name_value* env,
+        int count, const char* name)
+{
+    int         i;
+
+    for (i = 0; i < count; ++i)
+        if (!strcmp(env[i].name, name)) /* found */
+            return i;
+
+    return -1;
+}
+
+typedef struct
+{
+    char*       filename;
+} configuration;
+
+typedef struct
+{
+    editorconfig_name_value*        name_values;
+    int                             current_value_count;
+    int                             max_value_count;
+} array_editorconfig_name_value;
+
+/* initialize array_editorconfig_name_value */
+static void array_editorconfig_name_value_init(
+        array_editorconfig_name_value* aenv)
+{
+    aenv->name_values = NULL;
+    aenv->max_value_count = 0;
+    aenv->current_value_count = 0;
+}
+
+static int array_editorconfig_name_value_add(
+        array_editorconfig_name_value* aenv,
+        const char* name, const char* value)
+{
+#define VALUE_COUNT_INITIAL      10
+#define VALUE_COUNT_INCREASEMENT 10
+    int         name_value_pos;
+    /* always use name_lwr but not name, since property names are case
+     * insensitive */
+    char        name_lwr[MAX_PROPERTY_NAME];
+    /* For the first time we came here, aenv->name_values is NULL */
+    if (aenv->name_values == NULL) {
+        aenv->name_values = (editorconfig_name_value*)malloc(
+                sizeof(editorconfig_name_value) * 2 * VALUE_COUNT_INITIAL);
+
+        if (aenv->name_values == NULL)
+            return -1;
+
+        aenv->max_value_count = VALUE_COUNT_INITIAL;
+        aenv->current_value_count = 0;
+    }
+
+
+    /* name_lwr is the lowercase property name */
+    ec_strlwr(strcpy(name_lwr, name));
+
+    name_value_pos = find_name_value_from_name(
+            aenv->name_values, aenv->current_value_count, name_lwr);
+
+    if (name_value_pos >= 0) { /* current name has already been used */
+        free(aenv->name_values[name_value_pos].value);
+        set_name_value(&aenv->name_values[name_value_pos],
+                (const char*)NULL, value);
+        return 0;
+    }
+
+    /* if the space is not enough, allocate more before add the new name and
+     * value */
+    if (aenv->current_value_count >= aenv->max_value_count) {
+
+        editorconfig_name_value*        new_values;
+        int                             new_max_value_count;
+
+        new_max_value_count = aenv->current_value_count +
+            VALUE_COUNT_INCREASEMENT;
+        new_values = (editorconfig_name_value*)realloc(aenv->name_values,
+                sizeof(editorconfig_name_value) * 2 * new_max_value_count);
+
+        if (new_values == NULL) /* error occured */
+            return -1;
+
+        aenv->name_values = new_values;
+        aenv->max_value_count = new_max_value_count;
+    }
+
+    set_name_value(&aenv->name_values[aenv->current_value_count],
+            name_lwr, value);
+    ++ aenv->current_value_count;
+
+    return 0;
+#undef VALUE_COUNT_INITIAL
+#undef VALUE_COUNT_INCREASEMENT
+}
+
+typedef struct
+{
+    configuration                   conf;
+
+    array_editorconfig_name_value   array_name_value;
+} handler_first_param;
+
+/*
  * Accept INI property value and store known values in handler_first_param
  * struct.
  */
 static int handler(void* hfp, const char* section, const char* name,
         const char* value)
 {
-#define VALUE_COUNT_INITIAL      10
-#define VALUE_COUNT_INCREASEMENT 10
 
     handler_first_param* hfparam = (handler_first_param*)hfp;
 
     if (ec_fnmatch(section, hfparam->conf.filename, EC_FNM_PATHNAME) == 0) {
-        int         name_value_pos;
-        /* always use name_lwr but not name, since property names are case
-         * insensitive */
-        char        name_lwr[MAX_PROPERTY_NAME];
-
-        /* name_lwr is the lowercase property name */
-        ec_strlwr(strcpy(name_lwr, name));
-
-        /* For the first time we came here, hfparam->out_values is NULL */
-        if (hfparam->out_values == NULL) {
-            hfparam->out_values = (editorconfig_name_value*)malloc(
-                    sizeof(editorconfig_name_value) * 2 * VALUE_COUNT_INITIAL);
-
-            if (hfparam->out_values == NULL)
-                return 0;
-
-            hfparam->max_value_count = VALUE_COUNT_INITIAL;
-            hfparam->current_value_count = 0;
-        }
-
-        name_value_pos = find_name_value_from_name(
-                hfparam->out_values, hfparam->current_value_count, name_lwr);
-
-        if (name_value_pos >= 0) { /* current name has already been used */
-            free(hfparam->out_values[name_value_pos].value);
-            set_name_value(&hfparam->out_values[name_value_pos],
-                    (const char*)NULL, value);
-            return 1;
-        }
-
-        /* if the space is not enough, allocate more before add the new name
-         * and value */
-        if (hfparam->current_value_count >= hfparam->max_value_count) {
-            editorconfig_name_value*        new_values;
-            int                             new_max_value_count;
-            
-            new_max_value_count = hfparam->current_value_count +
-                VALUE_COUNT_INCREASEMENT;
-            new_values = (editorconfig_name_value*)realloc(hfparam->out_values,
-                    sizeof(editorconfig_name_value) * 2 * new_max_value_count);
-
-            if (new_values == NULL) /* error occured */
-                return 0;
-
-            hfparam->out_values = new_values;
-            hfparam->max_value_count = new_max_value_count;
-        }
-
-        set_name_value(&hfparam->out_values[hfparam->current_value_count],
-                name_lwr, value);
-        ++ hfparam->current_value_count;
+        if (array_editorconfig_name_value_add(&hfparam->array_name_value, name,
+                value))
+            return 0;
     }
 
     return 1;
-#undef VALUE_COUNT_INITIAL
-#undef VALUE_COUNT_INCREASEMENT
 }
 
 /* 
@@ -249,6 +275,8 @@ int editorconfig_parse(const char* full_filename,
 
     hfp.conf.filename = filename;
 
+    array_editorconfig_name_value_init(&hfp.array_name_value);
+
     config_files = get_filenames(full_filename2, "/.editorconfig");
     for (config_file = config_files; *config_file != NULL; config_file++) {
         if ((err_num = ini_parse(*config_file, handler, &hfp)) != 0 &&
@@ -265,8 +293,8 @@ int editorconfig_parse(const char* full_filename,
         free(*config_file);
     }
 
-    out->count = hfp.current_value_count;
-    out->name_values = hfp.out_values;
+    out->count = hfp.array_name_value.current_value_count;
+    out->name_values = hfp.array_name_value.name_values;
     out->name_values = realloc(      /* realloc to truncate the unused spaces */
             out->name_values, sizeof(editorconfig_name_value) * out->count);
     if (out->name_values == NULL) {
