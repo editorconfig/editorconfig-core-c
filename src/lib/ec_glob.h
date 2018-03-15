174688b41632ef69d8bb8b4a01085f35f50ef52d
Hong Xu <hong@topbug.net>
2018-03-15 14:01:11 -0700

    Release version 0.12.2.

--- src/lib/ec_glob.h
+++ src/lib/ec_glob.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hong Xu <hong AT topbug DOT net>
+ * Copyright (c) 2014-2018 Hong Xu <hong AT topbug DOT net>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
